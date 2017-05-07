debd1f5633ecf7ce96cf5f00f29549149f34745f
MertOnar <merttonar@gmail.com>
2017-05-07 21:45:38 +0300

    Update ourKonnektorDriver.java

--- ourKonnektorDriver.java
+++ ourKonnektorDriver.java
@@ -1,6 +1,18 @@
 package connectto;
 
-public class ourKonnektorDriver {
+import java.util.ArrayList;
 
+public class ourKonnektorDriver {
+	public static void main(String[] args)	{
 	ourKonnektor o=new ourKonnektor();
+	o.insertItem("hello","bu kalbimden daha temiz sayfayı sana açıyorum, mesgulsun herhalde cnm..");
+	ArrayList<datamodel>d=o.getitems();
+	for(datamodel d2:d)
+	{
+		System.out.println(d2.topic);
+		System.out.println("!!!!!!!");
+		System.out.println(d2.getContent());
+		System.out.println("----");
+	}
 }
+	}
