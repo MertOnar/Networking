f1707717451f3593efa9970140acfd240af4273a
MertOnar <merttonar@gmail.com>
2017-05-07 21:46:41 +0300

    Update ourKonnektor.java

--- ourKonnektor.java
+++ ourKonnektor.java
@@ -24,11 +24,50 @@ public class ourKonnektor {
 		Class.forName("com.mysql.jdbc.Driver");
         // Setup the connection with the DB
         connect = DriverManager
-                        .getConnection("jdbc:mysql://localhost/feedback?"
-                                        + "user=sqluser&password=sqluserpw");
+                        .getConnection("jdbc:mysql://localhost/myblog?"
+                                        + "user=root");
 		}catch(ClassNotFoundException | SQLException e){
 			e.printStackTrace();
 		}
 		
 	}
+	public void insertItem(String topic, String content)
+	{
+		 PreparedStatement preparedStatement = null;
+		  try {
+			preparedStatement = connect
+			          .prepareStatement("insert into myblog values (default, ?, ?)");
+			 preparedStatement.setString(1, topic);
+			 preparedStatement.setString(2, content);
+			 preparedStatement.executeUpdate();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}//insertitem
+	public ArrayList<datamodel> getitems()
+	{
+		ArrayList<datamodel>dummy=new ArrayList<datamodel>();
+		PreparedStatement preparedStatement2;
+		try {
+			preparedStatement2 = connect
+			          .prepareStatement("select * from myblog");
+			ResultSet results=preparedStatement2.executeQuery();
+			while(results.next())
+			{
+				datamodel d=new datamodel();
+				d.setTopic(results.getString("topic"));
+				d.setContent(results.getString("content"));
+				dummy.add(d);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return dummy;
+		
+		
+	}
+	
 }
